3b75f8c4519ca2c87cb997b3bb6e564567423666
Ali Gholami <aligholami7596@gmail.com>
2018-02-02 18:53:24 +0330

    Initial variables were added

--- src/drawer.py
+++ src/drawer.py
@@ -12,10 +12,21 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Load the dataset
 import tensorflow.examples.tutorials.mnist as input_data
 from tensorflow.examples.tutorials.mnist import mnist
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
+# Load the dataset
 MNIST = read_data_sets('MNIST_data')
+
+# Reset graph sessions on the RAM
+tf.reset_default_graph()
+
+BATCH_SIZE = 64
+
+# Batches of MNIST Characters
+X_INPUT = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28], name='X')
+Y_OUTPUT = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28], name='Y')
+Y_FLATTENED = tf.reshape(Y_OUTPUT, shape=[-1, 28*28])
+KEEP_PROB = tf.placeholder(dtype=tf.float32, shape=(), name='KEEP_PROB')
+
